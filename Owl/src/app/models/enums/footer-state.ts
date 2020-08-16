6950ba299e38dbd496d423d0ed1654f019849875
Vermero <julio_bermejoos@hotmail.com>
2020-08-16 20:44:47 -0300

    0.1.1

---
+++ Owl/src/app/models/enums/footer-state.ts
@@ -0,0 +1,18 @@
+/**
+ * O enum que representa os tipos
+ */
+
+ export enum FooterState{
+    
+    //Diz o menu de "categorias" está selecionado    
+    CATEGORIES = 0,
+
+    
+    //Diz o menu de "comentários" está selecionado    
+    ALL_COMMENTS = 1,
+    
+    
+    //Diz o menu de "meus comentários" está selecionado     
+    MY_COMMENTS = 2,
+
+}
--- Owl/src/app/pages/main/main.page.ts
+++ Owl/src/app/pages/main/main.page.ts
@@ -1,28 +1,34 @@
 //#region Imports
-
 import { Component } from '@angular/core';
-
+import { FooterState } from 'src/app/models/enums/footer-state';
 //#endregion
 
-/**
- * A classe que representa a página inicial da aplicação.
- */
 
+//A classe que representa a página inicial da aplicação.//
 @Component({
   selector: 'owl-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
+
+
 export class MainPage {
 
- //#region Constructor
-
- /**
-  * Constructor padrão
-  */
+  //#region Constructor
   
+  //Constructor padrão// 
   constructor(){}
+  
+  //#endregion
 
+  //#region Public Properties
+  
+  //Footer atualmente selecionado.//
+  public currentSelectedFooter: FooterState.CATEGORIES;
+
+  //Os estados possíveis para a parra de navegação do footer.//
+  public footerState: typeof FooterState = FooterState;
+  
   //#endregion
 
 }
